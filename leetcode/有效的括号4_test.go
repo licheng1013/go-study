b1f1858db6bc6046b08ced5083ca7d1ebd1b2322
licheng1013 <2501093733@qq.com>
2023-06-02 18:44:45 +0800

    优化问题修复!

--- leetcode/有效的括号4_test.go
+++ leetcode/有效的括号4_test.go
@@ -13,9 +13,11 @@ import (
 func Test4(t *testing.T) {
 	// 定义括号
 	strList := "()[]{}"
-	str := "(xxx)aa{aa}{aa}aa" // TODO 存在问题
+	str := "(xxx){()}{aa}" // TODO 存在问题-嵌套括号无法识别,例如: (xxx){a()a}{aa}
 
 	current := make([]string, 0)
+	// 是否以符号开头
+	leftOk := false
 	for i, o := range str {
 		k := string(o)
 		//t.Logf("字符串: %v", k)
@@ -23,6 +25,13 @@ func Test4(t *testing.T) {
 		if i == 0 && index == -1 {
 			t.Log("非法字符串: ", k)
 		}
+		if leftOk && index == -1 {
+			t.Logf("闭合错误:  %s-%s", current, str[0:i+1])
+			return
+		} else {
+			leftOk = false
+		}
+
 		if index != -1 {
 			if index == 0 || index == 2 || index == 4 {
 				// 左边括号
@@ -32,15 +41,14 @@ func Test4(t *testing.T) {
 				leftStrIndex := strings.Index(strList, current[len(current)-1])
 				// 当前是右阔后则
 				if leftStrIndex == 0 && index == 1 {
-					current = current[:len(current)-1]
 				} else if leftStrIndex == 2 && index == 3 {
-					current = current[:len(current)-1]
 				} else if leftStrIndex == 4 && index == 5 {
-					current = current[:len(current)-1]
 				} else {
-					t.Logf("闭合错误:  %s-%s", current, k)
+					t.Logf("闭合错误:  %s-%s", current, str[0:i+1])
 					return
 				}
+				current = current[:len(current)-1]
+				leftOk = true
 			}
 		}
 
