7f5889ffd1e23818d92493f0d59e5d42f7cc5fc6
licheng10113 <2501093733@qq.com>
2024-03-24 21:56:49 +0800

    这个监控!

---
+++ monitor/gui/gui.go
@@ -0,0 +1,22 @@
+package gui
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func Gui() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+}
--- monitor/main.go
+++ monitor/main.go
@@ -1,41 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/shirou/gopsutil/net"
-)
+import "monitor/gui"
 
 func main() {
-	var oldTotalBytesSent, oldTotalBytesRecv uint64
-
-	for {
-		netIOCounters, err := net.IOCounters(true)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-
-		var totalBytesSent, totalBytesRecv uint64
-		for _, netIOCounter := range netIOCounters {
-			totalBytesSent += netIOCounter.BytesSent
-			totalBytesRecv += netIOCounter.BytesRecv
-		}
-
-		if oldTotalBytesSent != 0 {
-			fmt.Printf("Upload speed: %d bytes/sec\n", totalBytesSent-oldTotalBytesSent)
-		}
-
-		if oldTotalBytesRecv != 0 {
-			fmt.Printf("Download speed: %d bytes/sec\n", totalBytesRecv-oldTotalBytesRecv)
-		}
-
-		// 一起打印
-
-		oldTotalBytesSent = totalBytesSent
-		oldTotalBytesRecv = totalBytesRecv
-
-		time.Sleep(1 * time.Second)
-	}
+	gui.Gui()
 }
